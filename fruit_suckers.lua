78a0bebd639d126aaac0e408fe62c3ae65c0649d
LioK <73439296+LioK251@users.noreply.github.com>
2023-03-26 23:10:36 +0900

    Update fruit_suckers.lua

--- fruit_suckers.lua
+++ fruit_suckers.lua
@@ -46,6 +46,99 @@ function getTools()
     return tools
 end
 
+function get_mob(name)
+    local mob, dist = nil, math.huge
+
+    if (not lp.Character and lp.Character:FindFirstChild("HumanoidRootPart")) then return end
+
+    for i,v in pairs(workspace:FindFirstChild("Mobs"):GetChildren()) do
+        if (not name or v.Name == name) then
+            local root = v:FindFirstChild("HumanoidRootPart")
+            local humanoid = v:FindFirstChildWhichIsA("Humanoid")
+
+            if (root and humanoid and humanoid.Health > 0) then
+                local mag = (lp.Character:WaitForChild("HumanoidRootPart").Position - root.Position).magnitude
+                
+                if (mag < dist) then
+                    dist = mag
+                    mob = v
+                end
+            end
+        end
+    end
+
+    return mob
+end
+
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                end)
+                wait(4)
+            end
+        end
+    end
+end
+
+function Teleport()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
+
 -- main tab
 
 mainTab.Label({Text = "Farm"})
@@ -67,15 +160,15 @@ mainTab.Toggle({Text = "Auto TP Mob", Callback = function(t)
         while auto_tp do task.wait()
             if not char:IsDescendantOf(lp.Character.Parent) or not char:FindFirstChild("HumanoidRootPart") or not char then char = lp.Character wait(0.5) end
 
-            for i,v in pairs(workspace:FindFirstChild("Mobs"):GetChildren()) do
-                if v.Name == npc_name and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") then
-                    repeat task.wait()
-                        lp.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(v:GetPivot().Position + Vector3.new(0,distance_from_mob,0))
-                    until auto_tp == false or v:FindFirstChild("Humanoid").Health <= 0
+            local target_mob = get_mob(npc_name)
 
-                    if v:FindFirstChild("Humanoid").Health <= 0 then
-                        lp.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(1216.3323974609, 1712.4995117188, -7681.0712890625)
-                    end
+            if (target_mob) then
+                repeat task.wait()
+                    lp.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(target_mob:GetPivot().Position + Vector3.new(0,distance_from_mob,0))
+                until auto_tp == false or target_mob:FindFirstChild("Humanoid").Health <= 0
+
+                if target_mob:FindFirstChild("Humanoid").Health <= 0 then
+                    lp.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(1216.3323974609, 1712.4995117188, -7681.0712890625)
                 end
             end
         end
@@ -97,6 +190,22 @@ mainTab.Toggle({Text = "Auto Quest", Callback = function(t)
                 game:GetService("ReplicatedStorage").Remotes.QuestRemote:FireServer("GetQuest", npc_name)
             end
             task.wait(.4)
+        end
+    end, t)
+end})
+
+mainTab.Toggle({Text = "Hide Name", Callback = function(t)
+    task.spawn(function(t)
+        getgenv().hide_name = t
+
+        while hide_name do task.wait()
+            if not char:IsDescendantOf(lp.Character.Parent) or not char:FindFirstChild("HumanoidRootPart") or not char then char = lp.Character wait(0.5) end
+
+            if (lp.Character:WaitForChild("Head").MobGUI.GUI.Info:FindFirstChild("NPCName")) then
+                lp.Character:WaitForChild("Head").MobGUI.GUI.Info:FindFirstChild("NPCName"):Destroy()
+            elseif (lp.Character:WaitForChild("Head").MobGUI.BountyUI:FindFirstChild("Bounty")) then
+                lp.Character:WaitForChild("Head").MobGUI.BountyUI:FindFirstChild("Bounty"):Destroy()
+            end
         end
     end, t)
 end})
@@ -161,6 +270,10 @@ miscTab.Button({Text = "Collect all Fruits", function()
             fireproximityprompt(v:FindFirstChild(v.Name).PromptAttachment.ProximityPrompt, 7)
         end
     end
+end})
+
+miscTab.Button({Text = "Server Hop", function()
+    Teleport()
 end})
 
 game:GetService('RunService').Stepped:Connect(function()
